07e9a41dcf62e50b1e691604bfe250564d3a060e
Jawann <jawann@gmail.com>
2017-12-09 21:29:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ require_relative 'concerns/findable'
 
 class Song
   extend Memorable
+  extend Findable
   attr_accessor :name
   attr_reader :artist
 
@@ -11,10 +12,6 @@ class Song
 
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def artist=(artist)
